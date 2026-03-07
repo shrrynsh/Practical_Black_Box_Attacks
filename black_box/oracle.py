c230946999eb65268c7082825c0bde08fb7a98d5
shrrynsh <shrynshmodi@gmail.com>
2026-03-07 11:16:30 +0000

    add: initialize BlackBoxModel and implement prediction function in oracle.py

--- black_box/oracle.py
+++ black_box/oracle.py
@@ -7,3 +7,15 @@ from .model import BlackBoxModel
 device=torch.device("cuda" if torchcuda.is_available() else 'cpu')
 
 MODEL_PATH ="/opt/watchdog/users/shreyansh/adv_diff/adv_ml/Practical_Black_Box_Attacks/black_box/blackbox.pt"
+
+ORACLE=BlackBoxModel()
+ORACLE.load_state_dict(torch.load(MODEL_PATH))
+
+ORACLE.to(device)
+ORACLE.eval()
+
+
+def get_orcale_predictions(x : Tensor):
+
+    x=x.to(device)
+    return ORACLE(x).argmax(dim=-1)
