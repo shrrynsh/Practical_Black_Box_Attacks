37e213ad063a4739f2b1b879515042bec86deaa6
shrrynsh <shrynshmodi@gmail.com>
2026-03-07 12:27:44 +0000

    add: implement data preparation and loading for MNIST in LightningMNIST class

--- black_box/train.py
+++ black_box/train.py
@@ -83,4 +83,29 @@ class LightningMNIST(LightningModule):
 
 
     def prepare_data(self):
-        
+        MNIST(self.data_dir,train=True,download=True)
+        MNIST(self.data_dir,train=False,download=True)
+
+
+    def setup(self,stage=None):
+
+        if stage=='fit' or stage is None:
+            mnist_full=MNIST(self.data_dir,train=True,transform=self.trasform)
+            self.mnist_train,self.mnist_val=random_split(mnist_full,[55000,5000])
+
+        if stage=='test' or stage is None:
+            self.mnist_test=MNIST(self.data_dir,train=False,transform=self.trasform)
+
+
+    def train_dataloader(self):
+        return DataLoader(self.mnist_train,batch_size=self.batch_size,num_workers=4)
+
+
+    def test_dataloader(self):
+        return DataLoader(self.mnist_test,batch_size=self.batch_size,num_workers=4)
+
+    def val_dataloader(self):
+        return DataLoader(self.mnist_val,batch_size=self.batch_size,num_workers=4)
+
+
+    
