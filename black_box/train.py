d728896e49dbc38b0dd51d0764c16699cd508152
shrrynsh <shrynshmodi@gmail.com>
2026-03-07 12:34:59 +0000

    fix: correct transform variable name in MNIST dataset loading

--- black_box/train.py
+++ black_box/train.py
@@ -94,7 +94,7 @@ class LightningMNIST(LightningModule):
             self.mnist_train,self.mnist_val=random_split(mnist_full,[55000,5000])
 
         if stage=='test' or stage is None:
-            self.mnist_test=MNIST(self.data_dir,train=False,transform=self.trasform)
+            self.mnist_test=MNIST(self.data_dir,train=False,transform=self.transform)
 
 
     def train_dataloader(self):
@@ -106,6 +106,23 @@ class LightningMNIST(LightningModule):
 
     def val_dataloader(self):
         return DataLoader(self.mnist_val,batch_size=self.batch_size,num_workers=4)
+
+
+
+def cli_main():
+    # The LightningCLI removes all the boilerplate associated with arguments parsing. This is purely optional.
+    cli = LightningCLI(
+        LightningMNIST,
+        seed_everything_default=42,
+        save_config_overwrite=True,
+        run=False,
+    )
+    cli.trainer.fit(cli.model, datamodule=cli.datamodule)
+    cli.trainer.test(ckpt_path="best", datamodule=cli.datamodule)
+
+
+if __name__ == "__main__":
+    cli_main()
 
 
     
