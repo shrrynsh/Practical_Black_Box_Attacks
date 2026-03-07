f7bf36350eeafd8984acbed16f8427781aeb09ed
shrrynsh <shrynshmodi@gmail.com>
2026-03-07 18:21:44 +0000

    add: implement training loop and Jacobian data augmentation in SubstituteModel class

--- substitute/model.py
+++ substitute/model.py
@@ -57,4 +57,93 @@ class SubstituteModel(nn.Model):
 
 
         def _fit_batch(self,images_batches,class_batch):
-            
+            images_batch,class_batch=images_batch.to(device),class_batch.to(device)
+            self.optimizer.zero_grad()
+            pred_batch=self(images_batch)
+            loss=self.get_loss(pred_batch,class_batch)
+            loss.backward()
+            self.optimizer.step()
+
+            return loss 
+
+
+
+        def train_epoch(
+            self,train_data :DataLoader,epoch :int,batch_size: Optionl[int]=None
+        ) ->  float:
+
+        self.train()
+        desc=f"Epoch{epoch}"
+        total=len(train_data)*batch_size if batch_size else len(train_data)
+        bar_fmt = "{l_bar}{bar}| [{elapsed}<{remaining}{postfix}]"
+
+        trn_loss=0
+        trn_done=0
+
+        pbar=tqdm(
+            trian_data,
+            desc=desc,
+            total=total,
+            leave=False,
+            miniters=1,
+            unit_scale=True,
+            bar_format=bar_fmt,
+            position=1,
+        )
+
+        for imgs,labels in pbar:
+            loss=self._fit_batch(images,labels)
+            trn_loss+=loss.item()*images.shape[0]
+            trn_done+=images.shape[0]
+
+
+    
+
+    def train_model(
+        self,
+        train_data :DataLoader,
+        epochs: int,
+        lr :float=1e-3,
+        batch_size :Optional[int]=None,
+
+    ) -> float:
+
+
+        self.add_optimizer(lr)
+        trnbar_fmt = "{l_bar}{bar}| [{elapsed}<{remaining}, {rate_fmt}{postfix}]"
+        pbar = tqdm(
+            range(epochs),
+            desc="Training",
+            total=epochs,
+            leave=False,
+            unit="epoch",
+            position=0,
+            bar_format=trnbar_fmt,
+        )
+
+
+        for epoch in pbar:
+            train_loss=self.train_epoch(trian_data,epoch)
+            pbar.set_postfix({"loss": "%.4g" % (train_loss)})
+
+
+    def jacobian_data_augmentation(
+        self,substitute_dataset: Dataset, p: int,lambda_:float, root_dir:str
+    )  -> None;
+
+        if not os.path.exists(root_dir):
+            os.mkdir(root_dir)
+
+        for i in trange(
+            len(substitute_dataset), desc="Jacobian dataset augmentation", leave=False
+        ):
+            image, label = substitute_dataset.__getitem__(i)
+            image, label = image.to(device), label.to(device)
+
+            # The Jacobian has shape 10 x 28 x 28
+            jacobian = torch.autograd.functional.jacobian(self, image.unsqueeze(dim=0)).squeeze()
+            new_image = image + lambda_ * torch.sign(jacobian[label])
+
+            # We save the tensors, some information was lost when saved as an image
+            torch.save(image, f"{root_dir}/{i}.pt")
+            torch.save(new_image, f"{root_dir}/{i + len(substitute_dataset)}.pt")
