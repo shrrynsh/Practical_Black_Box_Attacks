95888500562ead0854f6b0219b714bf92ff7f372
shrrynsh <shrynshmodi@gmail.com>
2026-03-08 07:25:11 +0000

    fix: correct typo in device initialization for CUDA availability check

--- black_box/oracle.py
+++ black_box/oracle.py
@@ -4,7 +4,7 @@ from torch import Tensor
 
 from .model import BlackBoxModel
 
-device=torch.device("cuda" if torchcuda.is_available() else 'cpu')
+device=torch.device("cuda" if torch.cuda.is_available() else 'cpu')
 
 MODEL_PATH ="/opt/watchdog/users/shreyansh/adv_diff/adv_ml/Practical_Black_Box_Attacks/black_box/blackbox.pt"
 
